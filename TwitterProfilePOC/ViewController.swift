63e54bc6ea836216d1ff8110b8bb1ce06b247005
Sohail-ur-rahman <ssohailmails@gmail.com>
2019-08-23 21:08:55 +0530

    Initial Commit

---
+++ TwitterProfilePOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterProfilePOC
+//
+//  Created by sohail ur rahman on 23/08/19.
+//  Copyright © 2019 sohail ur rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
